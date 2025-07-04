106663c6b9609e9156bb1f374073d4f0c99534d4
rionaldoriskysetyawan4 <rionaldoriskysetyawan4@gmail.com>
2025-07-04 16:07:39 +0700

    fixx

--- routes/sensor.js
+++ routes/sensor.js
@@ -6,8 +6,8 @@ router.post("/", async (req, res) => {
   try {
     const { temperature, humidity, timestamp } = req.body;
     const result = await db.query(
-      "INSERT INTO sensor_data (temperature, humidity, timestamp) VALUES ($1, $2, $3) RETURNING *",
-      [temperature, humidity, timestamp]
+ "INSERT INTO sensor_data (temperature, humidity, pump_out, timestamp) VALUES ($1, $2, $3, $4)",
+  [temperature, humidity, pump_out, timestamp]
     );
     res.status(201).json(result.rows[0]);
   } catch (error) {
