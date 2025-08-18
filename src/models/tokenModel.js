949f0ae50230b8cbbb928c50125977ef936abb77
rionaldoriskysetyawan4 <rionaldoriskysetyawan4@gmail.com>
2025-08-19 02:21:22 +0700

    fix: refactor token model to use pg for database operations and implement add, get, and delete token functions

--- src/models/tokenModel.js
+++ src/models/tokenModel.js
@@ -1,12 +1,28 @@
-const { DataTypes } = require("sequelize");
-const sequelize = require("../db");
+const pg = require('../db');
 
-const Token = sequelize.define("Token", {
-  token: {
-    type: DataTypes.TEXT,
-    allowNull: false,
-    unique: true,
-  },
-});
+// Add token
+async function addToken(token) {
+  const query = 'INSERT INTO tokens (token) VALUES ($1) ON CONFLICT DO NOTHING RETURNING *';
+  const values = [token];
+  const res = await pg.query(query, values);
+  return res.rows[0]; // data token yang baru
+}
 
-module.exports = Token;
+// Get all tokens
+async function getAllTokens() {
+  const res = await pg.query('SELECT * FROM tokens');
+  return res.rows;
+}
+
+// Delete token
+async function deleteToken(token) {
+  const query = 'DELETE FROM tokens WHERE token = $1';
+  const values = [token];
+  await pg.query(query, values);
+}
+
+module.exports = {
+  addToken,
+  getAllTokens,
+  deleteToken
+};
