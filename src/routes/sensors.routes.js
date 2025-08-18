4b99b885da0aad2a3518259eeff832a02da672bf
rionaldoriskysetyawan4 <rionaldoriskysetyawan4@gmail.com>
2025-08-19 01:00:59 +0700

    fix: correct import path for saveSensorData in sensors.routes.js

--- src/routes/sensors.routes.js
+++ src/routes/sensors.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { saveSensorData } = require("../controllers/sensorController");
+const { saveSensorData } = require("../controllers/sensorsController");
 
 // endpoint terima data dari ESP32
 router.post("/telemetry", saveSensorData);
